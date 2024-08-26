5d986900c0334d6878ab083bcbbd5332f1d06d4e
nidkit1234forex <nidkit1234forex@gmail.com>
2024-08-26 21:40:51 +0700

    Update begin.sh

--- begin.sh
+++ begin.sh
@@ -7,21 +7,11 @@ yes | pkg install libjansson wget nano
 cp /data/data/com.termux/files/usr/include/linux/sysctl.h /data/data/com.termux/files/usr/include/sys
 
 # Clone repository 
-git clone https://github.com/nidkit1234forex/jk8180-update.git
-cd jk8180-update
-mkdir set-miner-off
-mkdir set-miner-on
-chmod +x moveback.sh start-turbo.sh moveback-turbo.sh
-
-mkdir ccminer && cd ccminer
-wget https://raw.githubusercontent.com/Darktron/pre-compiled/a53/ccminer
-chmod +x ccminer 
-cd ~
 git clone https://github.com/nidkit1234forex/jk8180.git
 cd jk8180
 
 git clone https://github.com/nidkit1234forex/set-miner-off.git
-chmod +x start.sh move.sh move-turbo.sh
+chmod +x start.sh
 
 mkdir ccminer && cd ccminer
 wget https://raw.githubusercontent.com/Darktron/pre-compiled/a53/ccminer
