38bc937845099b0f7e008b4eca910023f3c561de
nidkit1234forex <nidkit1234forex@gmail.com>
2024-08-28 19:30:22 +0700

    Update begin.sh

--- begin.sh
+++ begin.sh
@@ -6,14 +6,12 @@ yes | pkg install libjansson wget nano
 
 cp /data/data/com.termux/files/usr/include/linux/sysctl.h /data/data/com.termux/files/usr/include/sys
 
-mv ~/jk8180/set-miner-off ~/
-
 # Clone repository 
 git clone https://github.com/nidkit1234forex/jk8180.git
 cd jk8180
 
 git clone https://github.com/nidkit1234forex/set-miner-off.git
-chmod +x start.sh start-fast.sh start-update.sh
+chmod +x start.sh 
 
 mkdir ccminer && cd ccminer
 wget https://raw.githubusercontent.com/Darktron/pre-compiled/a53/ccminer
